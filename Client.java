0ee1077420346e24008917603f6c030c175df42d
Karw1n <akbbalfour@gmail.com>
2023-11-12 16:25:52 +0000

    Added a way to read packet and write to file.

--- Client.java
+++ Client.java
@@ -1,3 +1,4 @@
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -16,18 +17,36 @@ public class Client{
 
     public static void main(String[] args) throws IOException{
         int port = 20777;
-        String ip = "192.168.86.199";
+        String ip = "127.0.0.1";
         DatagramSocket socket = null;
-        int bufferSize = 2048;
+        int bufferSize = 1460;
         byte[] buffer = new byte[bufferSize];
+        String fileName = "file.dat";
+        
         
         try {
-            InetAddress gamInetAddress = InetAddress.getByName(ip);
-            socket = new DatagramSocket(port, gamInetAddress);
-
+            InetAddress gameInetAddress = InetAddress.getByName(ip);
+            socket = new DatagramSocket(port, gameInetAddress);
             System.out.println("UDP Client started. Listening on port: " + port);
 
-        
+            while (true) {
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                socket.receive(packet);
+
+                // Process the received data
+                byte[] receivedData = packet.getData();
+                int length = packet.getLength();
+                // Process the received data here
+                FileOutputStream outputStream = new FileOutputStream(fileName);
+                outputStream.write(receivedData);
+                outputStream.close();
+
+                System.out.println("Data buffer has been written to the file in the current directory.");
+
+                // String receivedMessage = new String(receivedData, 0, length);
+                // System.out.println("Received data: " + receivedMessage);
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -35,7 +54,6 @@ public class Client{
                 socket.close();
             }
         }
-    
     }
 
 }
